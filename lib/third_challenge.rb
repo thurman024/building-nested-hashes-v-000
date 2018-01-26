9e8f5e7eb0ba21a603cdd218329798fae555ef5f
Stephen Thurman <thurman024@gmail.com>
2018-01-26 02:43:15 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,23 +10,30 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo"
+        :age => "15"
+        :status => "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet"
+        :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet"
+        :age => "51"
         },
       :heroine => {
+        :name => "Juliet"
+        :age => "15"
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
